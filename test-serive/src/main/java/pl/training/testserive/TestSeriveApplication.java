35b8ff7d09d1b7c272ecc89821dfa6393569676b
skynet <zcm@interia.pl>
2023-09-12 14:57:39 +0200

    add test

---
+++ test-serive/src/main/java/pl/training/testserive/TestSeriveApplication.java
@@ -0,0 +1,13 @@
+package pl.training.testserive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSeriveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSeriveApplication.class, args);
+    }
+
+}
