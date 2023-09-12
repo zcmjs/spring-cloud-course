2caa64d7ca15e9a49bb66203d4731be23304eb70
skynet <zcm@interia.pl>
2023-09-12 14:16:12 +0200

    test

--- ConfigurationServer/src/main/java/pl/traning/configurationServer/ConfigurationServerApplication.java
+++ ConfigurationServer/src/main/java/pl/traning/configurationServer/ConfigurationServerApplication.java
@@ -3,7 +3,6 @@ package pl.traning.configurationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigurationServerApplication {
