02fb829326d1872b5ba574d690a912da528a2c37
skynet <zcm@interia.pl>
2023-09-12 14:34:18 +0200

    add eureka server

---
+++ server-discover/src/main/java/pl/training/serverdiscover/ServerDiscoverApplication.java
@@ -0,0 +1,16 @@
+package pl.training.serverdiscover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class ServerDiscoverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerDiscoverApplication.class, args);
+    }
+
+}
---
+++ server-discover/src/test/java/pl/training/serverdiscover/ServerDiscoverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.training.serverdiscover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerDiscoverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
